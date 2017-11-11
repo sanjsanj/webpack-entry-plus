3c95407708af5ff2533c55da0243ed50407a83e4
sanjsanj <sanjsanj@hotmail.com>
2017-11-11 23:18:12 +0000

    refactor entry code

--- src/entry-plus.js
+++ src/entry-plus.js
@@ -2,9 +2,7 @@ const entryObject = params => params.reduce((acc, item) => {
   if (typeof item.outputName === 'function') {
     item.entryFiles.map(entry => acc[item.outputName(entry)] = entry);
   } else {
-    const name = item.outputName;
-    const value = item.entryFiles;
-    acc[name] = value;
+    acc[item.outputName] = item.entryFiles;
   }
   return acc;
 }, {});
